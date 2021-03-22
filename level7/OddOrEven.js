ab01866f1a5471b9d19acad9460567880b161dd1
Bobbi Henderson <bobbilhenderson@gmail.com>
2021-03-22 18:54:39 -0500

    Odd or Even challenge

---
+++ level7/OddOrEven.js
@@ -0,0 +1,23 @@
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+
+function oddOrEven(array) {
+    let total = 0;
+    if(array.length > 0){
+        total = array.reduce((acc,cur)=>(acc+cur))
+    }
+    return total === 0 || total % 2 === 0 ? 'even' : 'odd'
+}
