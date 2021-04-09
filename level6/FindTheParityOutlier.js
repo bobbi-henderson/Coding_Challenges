5c232406616ed8a4340ccd4700928dd20faaf7f1
Bobbi Henderson <bobbilhenderson@gmail.com>
2021-04-09 18:13:01 -0500

    reformat array into phone number

--- level6/FindTheParityOutlier.js
+++ level6/FindTheParityOutlier.js
@@ -11,4 +11,4 @@ function findOutlier(integers){
     let odd = integers.filter((x)=>{return x%2!==0})
     let even = integers.filter((x)=>{return x%2===0})
     return odd.length === 1 ? odd[0] : even[0]
-  }
+}
