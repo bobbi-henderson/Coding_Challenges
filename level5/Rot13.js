1689d8cc873e27d325f4ca1c952cf43f070d5970
Bobbi Henderson <bobbilhenderson@gmail.com>
2021-05-24 20:12:21 -0500

    reformat string to Jaden Case challenge

--- level5/Rot13.js
+++ level5/Rot13.js
@@ -2,17 +2,22 @@
 
 // Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
 
-function rot13(message){
-    let lower = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
-    let upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    let messageArr = message.split('').map((x,i)=>{
-      if(lower.includes(x)){
-        return lower[lower.indexOf(x)+13]
-      }else if(upper.includes(x)){
-        return upper[upper.indexOf(x)+13]
-      } else {
-        return x
-      }  
-    })
-    return messageArr.join('')
-  }
+function rot13(str) {
+  let lower = 'abcdefghijklmnopqrstuvwxyzabcdefghijklm'
+  let upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM'
+  return str.split('').map(x=>{
+    if(upper.includes(x)){
+      let index = upper.indexOf(x) + 13
+      return upper[index]
+    } else if(lower.includes(x)){
+      let index = lower.indexOf(x) + 13
+      return lower[index]
+    } else {
+      return x
+    }
+  }).join('')
+}
+
+console.log(rot13("EBG13 rknzcyr."))
+console.log(rot13("This is my first ROT13 excercise!"))
+console.log(rot13("otimq"))
